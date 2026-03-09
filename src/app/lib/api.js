48e1c47daadfe6e741e140f03133c7100c63035e
Luthfil4K <akaishuichi141516@gmail.com>
2026-03-09 08:42:01 +0800

    M: api clean

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
 
-console.log("process.env.NEXT_PUBLIC_LOCAL_URL")
-console.log("process.env.NEXT_PUBLIC_LOCAL_URL")
-console.log(process.env.NEXT_PUBLIC_LOCAL_URL)
-console.log(process.env.NEXT_PUBLIC_LOCAL_URL)
-console.log(process.env.NEXT_PUBLIC_LOCAL_URL)
-console.log("process.env.NEXT_PUBLIC_LOCAL_URL")
-
 const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_LOCAL_URL}/api`,
 });
